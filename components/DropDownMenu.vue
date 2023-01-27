f7332eeedaff6ac6922cdf22c91ff35d5acd8a78
Jeb <commit@jeb4.dev>
2023-01-27 16:29:15 +0200

    all base configs

--- components/DropDownMenu.vue
+++ components/DropDownMenu.vue
@@ -1,61 +1,52 @@
 <template>
-    <!-- Profile dropdown -->
-    <Menu as="div" class="relative">
-        <MenuButton
-            :class="[
-                menu.current
-                    ? 'bg-gray-900 text-white'
-                    : 'text-gray-700 dark:text-gray-300 hover:bg-gray-700 hover:text-white',
-                'flex items-center justify-between px-1 py-1 md:px-3 md:py-2 rounded-md font-medium uppercase md:font-bold text-sm lg:text-lg whitespace-nowrap',
-            ]"
-            :aria-current="menu.current ? 'page' : undefined"
-        >
-            {{ menu.name }}
-            <DropDownArrow />
-        </MenuButton>
-        <transition
-            enter-active-class="transition ease-out duration-100"
-            enter-from-class="transform opacity-0 scale-95"
-            enter-to-class="transform opacity-100 scale-100"
-            leave-active-class="transition ease-in duration-75"
-            leave-from-class="transform opacity-100 scale-100"
-            leave-to-class="transform opacity-0 scale-95"
-        >
-            <MenuItems
-                class="absolute right-0 z-10 mt-2 min-w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none whitespace-nowrap"
-            >
-                <MenuItem
-                    v-for="subMenu in menu.subMenu"
-                    v-slot="{ active }"
-                    :key="subMenu.name"
-                >
-                    <a
-                        href="#"
-                        :class="[
-                            active ? 'bg-gray-100' : '',
-                            'block px-4 py-2 text-sm text-gray-700',
-                        ]"
-                        >{{ subMenu.name }}</a
-                    >
-                </MenuItem>
-            </MenuItems>
-        </transition>
-    </Menu>
+   <!-- Profile dropdown -->
+   <Menu as="div" class="relative">
+      <MenuButton
+         :class="[
+            menu.current
+               ? 'bg-gray-900 text-white'
+               : 'text-gray-700 dark:text-gray-300 hover:bg-gray-700 hover:text-white',
+            'flex items-center justify-between px-1 py-1 md:px-3 md:py-2 rounded-md font-medium uppercase md:font-bold text-sm lg:text-lg whitespace-nowrap',
+         ]"
+         :aria-current="menu.current ? 'page' : undefined"
+      >
+         {{ menu.name }}
+         <DropDownArrow />
+      </MenuButton>
+      <transition
+         enter-active-class="transition ease-out duration-100"
+         enter-from-class="transform opacity-0 scale-95"
+         enter-to-class="transform opacity-100 scale-100"
+         leave-active-class="transition ease-in duration-75"
+         leave-from-class="transform opacity-100 scale-100"
+         leave-to-class="transform opacity-0 scale-95"
+      >
+         <MenuItems
+            class="absolute right-0 z-10 mt-2 min-w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none whitespace-nowrap"
+         >
+            <MenuItem v-for="subMenu in menu.subMenu" v-slot="{ active }" :key="subMenu.name">
+               <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{
+                  subMenu.name
+               }}</a>
+            </MenuItem>
+         </MenuItems>
+      </transition>
+   </Menu>
 </template>
 
 <script setup>
-    import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
-    import DropDownArrow from '@/components/DropDownArrow.vue';
+   import { Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue';
+   import DropDownArrow from '@/components/DropDownArrow.vue';
 </script>
 
 <script>
-    export default {
-        name: 'DropDownMenu',
-        props: {
-            // eslint-disable-next-line vue/require-default-prop
-            menu: Object,
-        },
-    };
+   export default {
+      name: 'DropDownMenu',
+      props: {
+         // eslint-disable-next-line vue/require-default-prop
+         menu: Object,
+      },
+   };
 </script>
 
 <style scoped></style>
