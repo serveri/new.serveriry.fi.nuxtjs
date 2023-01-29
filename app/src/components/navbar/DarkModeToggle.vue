d2b6807dba19abedf2ed0bb42ce3c3b8c437ab23
Jeb <jesperkauppinen@gmail.com>
2023-01-29 02:31:01 +0200

    style btn

--- app/src/components/navbar/DarkModeToggle.vue
+++ app/src/components/navbar/DarkModeToggle.vue
@@ -2,15 +2,15 @@
    <button
       title="Change page theme between light and dark."
       tabindex="0"
-      class="m-4 w-20 h-10 rounded-full bg-yellow-100 dark:bg-gray-800 flex items-center transition duration-300 focus:outline-none shadow focus:ring-2"
+      class="m-6 w-20 h-10 sm:h-6 sm:w-12 rounded-full bg-gray-200 dark:bg-gray-800 flex items-center transition duration-300 focus:outline-none shadow focus:ring-2"
       @click="switchTheme()"
    >
       <span
          :id="'switch-toggle-' + id.id"
-         class="flex justify-center items-center w-12 h-12 relative rounded-full transition duration-500 transform bg-yellow-500 -translate-x-2 p-1 text-white"
+         class="flex justify-center items-center w-12 h-12 sm:w-8 sm:h-8 relative rounded-full transition duration-500 transform bg-gray-300 -translate-x-2 p-1 text-white"
          ><ClientOnly fallback-tag="span" fallback="">
             <font-awesome-icon :id="'icon-moon-' + id.id" class="hidden" :icon="['fas', 'moon']" />
-            <font-awesome-icon :id="'icon-sun-' + id.id" class="hidden" :icon="['fas', 'sun']" />
+            <font-awesome-icon :id="'icon-sun-' + id.id" class="hidden" :icon="['fas', 'sun']" style="color: rgb(55 65 81)" />
          </ClientOnly>
       </span>
    </button>
@@ -19,9 +19,7 @@
 <script setup lang="ts">
    import { useDark, useToggle } from '@vueuse/core';
    import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-   import { onMounted } from 'vue';
-
-   import { nextTick } from 'vue';
+   import { nextTick, onMounted } from 'vue';
 
    const id = defineProps({
       id: {
@@ -53,13 +51,13 @@
    }
 
    function dark(switchToggle: HTMLElement | null, iconSun: HTMLElement | null, iconMoon: HTMLElement | null) {
-      switchToggle?.classList.add('bg-yellow-500', '-translate-x-2');
+      switchToggle?.classList.add('bg-gray-300', '-translate-x-2');
       switchToggle?.classList.remove('bg-gray-700', 'translate-x-full');
       iconSun?.classList.remove('hidden');
       iconMoon?.classList.add('hidden');
    }
    function light(switchToggle: HTMLElement | null, iconSun: HTMLElement | null, iconMoon: HTMLElement | null) {
-      switchToggle?.classList.remove('bg-yellow-500', '-translate-x-2');
+      switchToggle?.classList.remove('bg-gray-300', '-translate-x-2');
       switchToggle?.classList.add('bg-gray-700', 'translate-x-full');
       iconSun?.classList.add('hidden');
       iconMoon?.classList.remove('hidden');
