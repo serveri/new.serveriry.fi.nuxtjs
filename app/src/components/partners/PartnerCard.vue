2818af0cd0e2a983f4b371f05a44e843e044ad59
Jeb <jesperkauppinen@gmail.com>
2023-01-30 00:20:58 +0200

    partners page

---
+++ app/src/components/partners/PartnerCard.vue
@@ -0,0 +1,56 @@
+<template>
+   <a
+      :href="partner.url"
+      class="rounded-2xl w-full h-full cursor-pointer dark:bg-zinc-900 dark:shadow-lg dark:shadow-zinc-600/50"
+      @mouseover="hover = true"
+      @mouseleave="hover = false"
+   >
+      <img v-if="hover" :src="partner.img" alt="hurja" loading="lazy" class="scale-110" />
+      <img v-else :src="partner.img" alt="hurja" loading="lazy" class="scale-100" />
+   </a>
+</template>
+
+<script setup lang="ts">
+   const partner = defineProps({
+      url: {
+         type: String,
+         default: 'Default',
+         required: true,
+      },
+      img: {
+         type: String,
+         default: 'Default',
+         required: true,
+      },
+   });
+</script>
+
+<script lang="ts">
+   export default {
+      name: 'PartnerCard',
+      data() {
+         return {
+            hover: false,
+         };
+      },
+   };
+</script>
+
+<style scoped>
+   a {
+      -webkit-box-shadow: 0 0 26px -5px rgba(0, 0, 0, 0.27);
+      -moz-box-shadow: 0 0 26px -5px rgba(0, 0, 0, 0.27);
+      box-shadow: 0 0 26px -5px rgba(0, 0, 0, 0.27);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+   }
+
+   img {
+      height: 7rem;
+      width: 90%;
+      padding: 0.8rem;
+      object-fit: contain;
+      -o-object-fit: contain;
+   }
+</style>
