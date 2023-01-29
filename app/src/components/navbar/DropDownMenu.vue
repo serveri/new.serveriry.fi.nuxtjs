473d5fc2197bc2a46ccddb686d794caf6d5ade66
Jeb <jesperkauppinen@gmail.com>
2023-01-30 00:21:53 +0200

    dark drop down + accessibility

--- app/src/components/navbar/DropDownMenu.vue
+++ app/src/components/navbar/DropDownMenu.vue
@@ -7,7 +7,7 @@
             :class="[
                menu.current
                   ? 'bg-gray-900 text-white'
-                  : 'text-gray-700 dark:text-gray-300 dark:hover:bg-gray-700 hover:outline outline-2 dark:hover:text-white hover:text-blue-500',
+                  : 'text-gray-700 dark:text-gray-300 dark:hover:bg-gray-700 hover:outline dark:hover:outline-none outline-2 dark:hover:text-white hover:text-blue-500',
                'flex items-center justify-between px-1 py-1 md:px-3 md:py-2 rounded-md font-medium uppercase md:font-bold text-sm lg:text-lg whitespace-nowrap',
             ]"
             :aria-current="menu.current ? 'page' : undefined"
@@ -25,12 +25,16 @@
          leave-to-class="transform opacity-0 scale-95"
       >
          <MenuItems
-            class="absolute right-0 z-10 mt-2 min-w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none whitespace-nowrap"
+            class="absolute right-0 z-10 mt-2 min-w-max origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none whitespace-nowrap dark:bg-[#282828]"
          >
             <MenuItem v-for="subMenu in menu.subMenu" v-slot="{ active }" :key="subMenu.name">
                <nuxt-link
                   :to="localePath(subMenu.href)"
-                  :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']"
+                  :class="[
+                     active ? 'bg-gray-100 dark:outline dark:outline-1' : '',
+                     'my-custom-one block px-4 py-2 text-sm text-gray-700 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:outline-none dark:even:bg-[#202020] dark:odd:bg-[#282828]',
+                  ]"
+                  tabindex="1"
                   >{{ $t(subMenu.name) }}</nuxt-link
                >
             </MenuItem>
