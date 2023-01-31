0aab3344edda34e9242094e9575115f4fbfcdece
Kalle <helvelahti.kalle@gmail.com>
2023-01-31 15:45:16 +0200

    added NewsCard

---
+++ app/src/components/news/NewsCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="uukortti">
+        <img src="" alt="ei löyvy">
+        <h1 class="korttiOtsikko">uutiskortti?</h1>
+        <div>
+            <p>Julkaistu:</p>
+            <p>111111111111111111111</p>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+   export default {
+      name: 'newsCard',
+      data() {
+         return {
+            hover: false,
+         };
+      },
+   };
+</script>
+
+<style>
+    .uukortti{
+     -webkit-box-shadow: 0 0 26px -5px rgba(0, 0, 0, 0.27);
+      -moz-box-shadow: 0 0 26px -5px rgba(0, 0, 0, 0.27);
+      box-shadow: 0 0 26px -5px rgba(0, 0, 0, 0.27);
+      border-radius: 15px;
+      padding: 0.8rem;   
+}
+.korttiOtsikko{
+    font-size: x-large;
+}
+
+p{
+    font-size: larger;
+}
+</style>
--- app/src/pages/yhdistys/uutiset.vue
+++ app/src/pages/yhdistys/uutiset.vue
@@ -4,5 +4,27 @@
          <Title>{{ $t('title_news') }} - Serveri ry</Title>
       </Head>
       <h1 class="custom-page-title">{{ $t('h1_news') }}</h1>
+         <div class="news pt-8">
+            <NewsCard/>      
+            <NewsCard/>  
+            <NewsCard/>  
+         </div>
+      
    </div>
+   
+   
+
 </template>
+
+<script>
+
+</script>
+
+
+<style>
+.news{
+   display: flex;
+   gap: 2rem;
+   grid-auto-rows: 8rem;
+}
+</style>
