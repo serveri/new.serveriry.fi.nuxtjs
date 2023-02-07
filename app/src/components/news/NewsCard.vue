3f7917bea309d26b8ff09c43a78ae4ea8db2327f
Jeb <commit@jeb4.dev>
2023-02-07 20:06:18 +0200

    prettier

--- app/src/components/news/NewsCard.vue
+++ app/src/components/news/NewsCard.vue
@@ -1,42 +1,42 @@
 <template>
-    <div class="uukortti">
-        <img :src="content.img" alt="Cover image for the news card">
-        <h1 class="korttiOtsikko">{{content.header}}</h1>
-        <div>
-            <p>{{ $t('news_released') }}</p>
-            <p>{{content.date}}</p>
-        </div>
-    </div>
+   <div class="uukortti">
+      <img :src="content.img" alt="Cover image for the news card" />
+      <h1 class="korttiOtsikko">{{ content.header }}</h1>
+      <div>
+         <p>{{ $t('news_released') }}</p>
+         <p>{{ content.date }}</p>
+      </div>
+   </div>
 </template>
 
 <script setup lang="ts">
-const content = defineProps({
-  url: {
-    type: String,
-    default: 'Default',
-    required: true,
-  },
-  img: {
-    type: String,
-    default: 'Default',
-    required: true,
-  },
-  header: {
-    type: String,
-    default: 'Default',
-    required: true,
-  },
-  date: {
-    type: String,
-    default: 'Default',
-    required: true,
-  },
-  text: {
-    type: String,
-    default: 'Default',
-    required: true,
-  },
-});
+   const content = defineProps({
+      url: {
+         type: String,
+         default: 'Default',
+         required: true,
+      },
+      img: {
+         type: String,
+         default: 'Default',
+         required: true,
+      },
+      header: {
+         type: String,
+         default: 'Default',
+         required: true,
+      },
+      date: {
+         type: String,
+         default: 'Default',
+         required: true,
+      },
+      text: {
+         type: String,
+         default: 'Default',
+         required: true,
+      },
+   });
 </script>
 <script lang="ts">
    export default {
@@ -50,35 +50,35 @@ const content = defineProps({
 </script>
 
 <style scoped>
-    .uukortti{
-    border-radius: 1em;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    box-shadow: 0 0 26px -5px rgba(0, 0, 0, 0.27);
-}
-h2{
-    padding-left: 1rem;
-    font-size: 1.6rem;
-    font-weight: 800;
-    margin-top: 0.8rem;
-    margin-bottom: 0.3rem;
-}
+   .uukortti {
+      border-radius: 1em;
+      width: 100%;
+      height: 100%;
+      overflow: hidden;
+      box-shadow: 0 0 26px -5px rgba(0, 0, 0, 0.27);
+   }
+   h2 {
+      padding-left: 1rem;
+      font-size: 1.6rem;
+      font-weight: 800;
+      margin-top: 0.8rem;
+      margin-bottom: 0.3rem;
+   }
 
-h5{
-    padding-left: 1rem;
-    font-weight: 400;
-    text-transform: uppercase;
-}
+   h5 {
+      padding-left: 1rem;
+      font-weight: 400;
+      text-transform: uppercase;
+   }
 
-p{
-    padding-left: 1rem  ;
-    margin-top: 0.5rem;
-    margin-bottom: 0.8rem;
-}
-img{
-    width: 100%;
-    object-fit: cover;
-    height: 15rem;
-}
+   p {
+      padding-left: 1rem;
+      margin-top: 0.5rem;
+      margin-bottom: 0.8rem;
+   }
+   img {
+      width: 100%;
+      object-fit: cover;
+      height: 15rem;
+   }
 </style>
--- app/src/pages/yhdistys/uutiset.vue
+++ app/src/pages/yhdistys/uutiset.vue
@@ -4,29 +4,23 @@
          <Title>{{ $t('title_news') }} - Serveri ry</Title>
       </Head>
       <h1 class="custom-page-title">{{ $t('h1_news') }}</h1>
-         <div class="news pt-8">
-            <NewsCard/>      
-            <NewsCard/>  
-            <NewsCard/>
-            <NewsCard/>
-            <NewsCard/>
-         </div>
+      <div class="news pt-8">
+         <NewsCard />
+         <NewsCard />
+         <NewsCard />
+         <NewsCard />
+         <NewsCard />
+      </div>
    </div>
-   
-   
-
 </template>
 
-<script>
-
-</script>
-
+<script></script>
 
 <style>
-.news{
-   padding-bottom: 3rem;
-   display: grid;
-   gap: 2rem;
-   grid-template-columns:auto auto auto;
-}
+   .news {
+      padding-bottom: 3rem;
+      display: grid;
+      gap: 2rem;
+      grid-template-columns: auto auto auto;
+   }
 </style>
