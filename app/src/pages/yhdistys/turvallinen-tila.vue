4730e366437104c47aad77e417165ab8f8300b53
Amim Mahad <aminmahadd@gmail.com>
2023-02-07 19:23:46 +0200

    code of conduct

--- app/src/pages/yhdistys/turvallinen-tila.vue
+++ app/src/pages/yhdistys/turvallinen-tila.vue
@@ -4,5 +4,47 @@
          <Title>{{ $t('title_safe-space') }} - Serveri ry</Title>
       </Head>
       <h1 class="custom-page-title">{{ $t('h1_safe-space') }}</h1>
+      
+
+<p>1. Respect and Inclusion:</p> 
+Members of the Serveri ry should respect and value each other's differences and promote a welcoming and inclusive environment for all individuals, regardless of race, gender, religion, sexual orientation, or ability.
+
+<p>2. Professionalism:</p>
+Members should behave in a professional manner and demonstrate good judgment and ethics, both in-person and online.
+
+<h3>3. Communication:</h3>
+<p>Members should communicate respectfully, even when they disagree. Derogatory language, bullying, and harassment will not be tolerated.</p>
+
+<p>4. Confidentiality:</p>
+Members should maintain confidentiality and not share confidential information unless they have been given permission.
+<br>
+
+<p>5. Responsibility:</p>
+Members are responsible for their actions and should be accountable for any consequences that result from their actions.
+<br>
+
+<p>6. Collaboration:</p>
+Members should collaborate and help each other whenever possible. They should encourage and support each other's growth and development.
+<br>
+
+<p>7. Intellectual Property:</p>
+Members should respect intellectual property rights and not plagiarize or use others' work without proper attribution.
+<br>
+
+<p>8. Conflicts of Interest:</p>
+<br>
+Members should disclose any conflicts of interest and recuse themselves from any discussions or decisions that may present a conflict.
+
+
+<P>By participating in the Serveri ry, members agree to adhere to this Code of Conduct and support a positive and respectful community. Any violations of this Code of Conduct may result in disciplinary action, up to and including removal from the Serveri ry.</P>
+
    </div>
 </template>
+
+<style>
+ h3 { 
+   font-weight: 700;
+   font-size: large;
+   margin: 1rem 0;
+}
+</style>
