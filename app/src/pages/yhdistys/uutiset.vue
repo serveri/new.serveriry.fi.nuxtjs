2eb740e0a152249f2edb1e8b2dc68ffb1f3d102a
Jeb <commit@jeb4.dev>
2023-02-07 19:11:52 +0200

    prettier

--- app/src/pages/yhdistys/uutiset.vue
+++ app/src/pages/yhdistys/uutiset.vue
@@ -4,27 +4,20 @@
          <Title>{{ $t('title_news') }} - Serveri ry</Title>
       </Head>
       <h1 class="custom-page-title">{{ $t('h1_news') }}</h1>
-         <div class="news pt-8">
-            <NewsCard/>      
-            <NewsCard/>  
-            <NewsCard/>  
-         </div>
-      
+      <div class="news pt-8">
+         <NewsCard />
+         <NewsCard />
+         <NewsCard />
+      </div>
    </div>
-   
-   
-
 </template>
 
-<script>
-
-</script>
-
+<script></script>
 
 <style>
-.news{
-   display: flex;
-   gap: 2rem;
-   grid-auto-rows: 8rem;
-}
+   .news {
+      display: flex;
+      gap: 2rem;
+      grid-auto-rows: 8rem;
+   }
 </style>
