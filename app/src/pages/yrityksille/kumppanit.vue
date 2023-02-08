d8d737ec58423e43f13c6e78e592129f71e609a1
Jeb <jesperkauppinen@gmail.com>
2023-02-09 00:41:00 +0200

    moved sponsors to own component

--- app/src/pages/yrityksille/kumppanit.vue
+++ app/src/pages/yrityksille/kumppanit.vue
@@ -11,51 +11,10 @@
          Tässä on viimeaikaiset tahot jotka ovat olleet esillä jossain tapahtumassamme tai haalareissamme:
       </p>
 
-      <div class="partners pt-8">
-         <partner-card
-            v-for="sponsor in sponsors"
-            :key="sponsor.name"
-            :url="sponsor.url"
-            :img="sponsor.img"
-            :alt="'Serverin toimintaa tukee ' + sponsor.name + ' ja tässä heidän logo'"
-         />
-      </div>
+      <partner-page-section />
    </div>
 </template>
 
 <script setup lang="ts">
-   import PartnerCard from '@/components/partners/PartnerCard.vue';
-
-   const sponsors = [
-      {
-         name: 'Efecte',
-         url: 'https://www.efecte.com/',
-         img: 'https://api.serveriry.fi/uploads/efecte_logo_eaa50a1bf4.png',
-      },
-      {
-         name: 'Innofactor',
-         url: 'https://www.innofactor.com/',
-         img: 'https://api.serveriry.fi/uploads/innofactor_365559a4a1.png',
-      },
-      {
-         name: 'TEK',
-         url: 'https://www.tek.fi/',
-         img: 'https://api.serveriry.fi/uploads/TEK_logo_slogan_CMYK_musta_3431_d41fe002b6.png',
-      },
-      {
-         name: 'Hurja',
-         url: 'https://www.hurja.fi/',
-         img: 'https://api.serveriry.fi/uploads/g9_1f987a430f.png',
-      },
-   ];
+   import PartnerPageSection from '@/components/partners/PartnerPageSection.vue';
 </script>
-
-<style>
-   .partners {
-      padding-bottom: 3rem;
-      display: grid;
-      gap: 2rem;
-      grid-auto-rows: 8rem;
-      grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
-   }
-</style>
