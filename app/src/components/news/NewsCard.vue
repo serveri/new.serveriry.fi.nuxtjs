354f35ba9b4e4bb07ae75cf23e0963811caf8ff1
Kalle Helvelahti <helvelahti.kalle@gmail.com>
2023-02-19 23:16:44 +0200

    aiemman pull-requestin ongelmat korjattu

--- app/src/components/news/NewsCard.vue
+++ app/src/components/news/NewsCard.vue
@@ -1,12 +1,14 @@
 <template>
    <a :href="'/news/' + content.url">
-      <div class="uukortti">
-         <img :src="content.img" alt="Cover image for the news card" />
-         <h1 class="korttiOtsikko">{{ content.header }}</h1>
-         <p>
+      <div class="NewsCard">
+         <div class="image-div">
+         <img class="news-image" :src="content.img" alt="Cover image for the news card" />
+         </div>
+         <h2 class="card-header">{{ content.header }}</h2>
+         <p class="news-date">
             {{ $t('news_released') }} <span>{{ content.date.toLocaleDateString($i18n.locale, options) }}</span>
          </p>
-         <p>{{ content.text }}</p>
+         <p class="card-content">{{ content.text }}</p>
       </div>
    </a>
 </template>
@@ -43,35 +45,40 @@
 </script>
 
 <style scoped>
-   .uukortti {
+   .NewsCard {
       border-radius: 1em;
       width: 100%;
       height: 100%;
       overflow: hidden;
-      box-shadow: 0 0 26px -5px rgba(0, 0, 0, 0.27);
+      box-shadow: 0 0 26px -5px #ccc;
+      -webkit-box-shadow: 0 0 26px -5px #ccc;
+      -moz-box-shadow: 0 0 26px -5px #ccc;
+      padding: 1rem;
    }
-   h2 {
-      padding-left: 1rem;
-      font-size: 1.6rem;
+   
+   
+   .card-header {
+      font-size: 1.4rem;
       font-weight: 800;
-      margin-top: 0.8rem;
-      margin-bottom: 0.3rem;
+      margin-top: 1rem;
    }
 
-   h5 {
-      padding-left: 1rem;
+   .news-date {
       font-weight: 400;
       text-transform: uppercase;
    }
 
-   p {
-      padding-left: 1rem;
+   .card-content {
       margin-top: 0.5rem;
-      margin-bottom: 0.8rem;
    }
-   img {
-      width: 100%;
+   .news-image {
       object-fit: cover;
       height: 15rem;
+      
+   }
+   .image-div{
+      margin: -1rem;
+      border-radius: 1em;
+      
    }
 </style>
