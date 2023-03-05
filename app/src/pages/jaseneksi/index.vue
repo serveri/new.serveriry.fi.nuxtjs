13a70c2d2d64789251d6ac2f8a12166350a41ecf
Jeb <jesperkauppinen@gmail.com>
2023-03-06 01:42:26 +0200

    prettify

--- app/src/pages/jaseneksi/index.vue
+++ app/src/pages/jaseneksi/index.vue
@@ -10,8 +10,12 @@
          <h1 class="custom-page-title">{{ $t('h1_join-us') }}</h1>
          <p class="text-center p-10 text-3xl">Come code with us :))</p>
          <p class="text-center p-10 text-3xl">Yeah...</p>
-         <p class="text-center p-10 text-3xl">This image takes 100% of screen height and width, something that WeMo course work is required to do...</p>
-         <p class="text-center p-10 text-3xl">But if you rly wanna join us, send email to <a href="mailto:pk@serveriry.fi">pj@serveriry.fi</a></p>
+         <p class="text-center p-10 text-3xl">
+            This image takes 100% of screen height and width, something that WeMo course work is required to do...
+         </p>
+         <p class="text-center p-10 text-3xl">
+            But if you rly wanna join us, send email to <a href="mailto:pk@serveriry.fi">pj@serveriry.fi</a>
+         </p>
       </div>
    </div>
 </template>
