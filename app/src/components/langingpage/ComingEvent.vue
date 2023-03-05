fd252ff3a4e2bb3336622baeda13ca0f781a07bc
Jeb <jesperkauppinen@gmail.com>
2023-03-05 22:29:04 +0200

    button color fixed

--- app/src/components/langingpage/ComingEvent.vue
+++ app/src/components/langingpage/ComingEvent.vue
@@ -6,7 +6,7 @@
       </p>
       <nuxt-link
          :to="event.link"
-         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-server-blue rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+         class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-server-blue rounded-lg hover:bg-cyan-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-server-blue dark:hover:bg-cyan-800 dark:focus:ring-blue-800"
       >
          Lue lisää
          <svg
