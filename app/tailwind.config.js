875ecfea580a6905824fc30daea75a4ded2b1ea1
Jeb <jesperkauppinen@gmail.com>
2023-03-12 01:14:53 +0200

    +custom-blue

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -12,8 +12,9 @@ module.exports = {
    theme: {
       extend: {
          colors: {
-            'server-blue': '#06919f',
             'uef-blue': '#00778a',
+            'server-blue': '#06919f',
+            'custom-blue': '#009fb8',
          },
          backgroundImage: {
             'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
