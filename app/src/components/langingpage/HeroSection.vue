886e47f75a37828dd3e641013d20b4cdcdb0cfd3
Jeb <jesperkauppinen@gmail.com>
2023-03-12 01:00:33 +0200

    removed gradient style

--- app/src/components/langingpage/HeroSection.vue
+++ app/src/components/langingpage/HeroSection.vue
@@ -2,17 +2,17 @@
    <div class="py-20 bg-cover bg-no-repeat bg-fixed bg-[url('/images/serveritoverit.png')] bg-cover h-[400px]">
       <div class="container m-auto text-center px-6 opacity-100">
          <h1 class="text text-3xl font-extrabold text-gray-900 dark:text-gray-900 md:text-5xl lg:text-6xl pb-2">
-            <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Serveri ry</span>
+            <span class="text-transparent bg-clip-text bg-server-blue">Serveri ry</span>
          </h1>
-         <h3 class="text-2xl md:mb-8 dark:text-black">
+         <h3 class="text-2xl md:mb-16 dark:text-black">
             Itä-Suomen yliopiston Kuopion kampuksen tietojenkäsittelytieteen opiskelijoiden ainejärjestö
          </h3>
          <nuxt-link to="/opiskelu/tkt">
-            <button
-               class="my-8 text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 shadow-lg shadow-teal-500/50 dark:shadow-teal-800/80 rounded-lg text-center mr-2 mb-2 rounded-full uppercase font-bold md:text-xl py-4 px-8 tracking-wider"
+            <span
+               class="py-4 px-8 text-white bg-server-blue hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 rounded-lg text-center rounded-full uppercase font-bold md:text-xl tracking-wider"
             >
                Millaista tietojenkäsittelytiede on?
-            </button>
+            </span>
          </nuxt-link>
       </div>
    </div>
