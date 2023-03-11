d39f7bbb64c0921241f6b83d67da009958c706ca
Jeb <jesperkauppinen@gmail.com>
2023-03-12 01:22:02 +0200

    edited button style

--- app/src/components/langingpage/HeroSection.vue
+++ app/src/components/langingpage/HeroSection.vue
@@ -2,14 +2,15 @@
    <div class="py-20 bg-cover bg-no-repeat bg-fixed bg-[url('/images/serveritoverit.png')] bg-cover h-[400px]">
       <div class="container m-auto text-center px-6 opacity-100">
          <h1 class="text text-3xl font-extrabold text-gray-900 dark:text-gray-900 md:text-5xl lg:text-6xl pb-2">
-            <span class="text-transparent bg-clip-text bg-server-blue">Serveri ry</span>
+            <span class="text-transparent bg-clip-text bg-custom-blue">Serveri ry</span>
          </h1>
          <h3 class="text-2xl md:mb-16 dark:text-black">
             Itä-Suomen yliopiston Kuopion kampuksen tietojenkäsittelytieteen opiskelijoiden ainejärjestö
          </h3>
-         <nuxt-link to="/opiskelu/tkt">
+         <nuxt-link to="/opiskelu/tkt" class="py-4">
             <span
-               class="py-4 px-8 text-white bg-server-blue hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 dark:focus:ring-teal-800 rounded-lg text-center rounded-full uppercase font-bold md:text-xl tracking-wider"
+               class="py-4 px-8 text-white bg-custom-blue hover:bg-uef-blue rounded-lg text-center uppercase font-bold md:text-xl tracking-wider"
+               role="button"
             >
                Millaista tietojenkäsittelytiede on?
             </span>
