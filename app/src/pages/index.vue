7b1fa10e6ed1f8528d0e4c268def8da1b04c68b5
Jeb <jesperkauppinen@gmail.com>
2023-03-11 16:27:46 +0200

    refactored "hero" stuff

--- app/src/pages/index.vue
+++ app/src/pages/index.vue
@@ -1,15 +1,13 @@
-import UutisetHero from '@/components/hero/UutisetHero.vue';
-
 <template>
    <div class="flex flex-col justify-center items-center">
       <Head>
          <Title>{{ $t('title_front-page') }} - Serveri ry</Title>
       </Head>
 
-      <HeroImage class="w-screen" />
+      <HeroSection class="w-screen" />
       <section class="flex flex-col py-5 lg:flex-row md:flex-col sm:flex-col">
-         <HeroSection />
-         <HeroUutiset />
+         <DescriptionText />
+         <TopNews />
       </section>
 
       <!-- 2 Col -->
@@ -27,6 +25,9 @@ import UutisetHero from '@/components/hero/UutisetHero.vue';
    import InstagramFeed from '@/components/langingpage/InstagramFeed.vue';
    import YTHS from '@/components/langingpage/yths.vue';
    import EmbeddedMap from '@/components/langingpage/EmbeddedMap.vue';
+   import HeroSection from '@/components/langingpage/HeroSection.vue';
+   import DescriptionText from '@/components/langingpage/DescriptionText.vue';
+   import TopNews from '@/components/langingpage/TopNews.vue';
 </script>
 <style>
    p {
