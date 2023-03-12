87e8ff2f8d90857b2faa02152e87198dfb61a676
Jeb <jesperkauppinen@gmail.com>
2023-03-12 02:34:34 +0200

    contact info + styling

--- app/src/components/langingpage/EmbeddedMap.vue
+++ app/src/components/langingpage/EmbeddedMap.vue
@@ -1,27 +1,39 @@
 <template>
-   <section class="relative w-[100vw]">
-      <div class="absolute w-[100vw] h-full bg-gray-300 dark:bg-gray-600">
+   <section class="relative w-screen">
+      <div class="absolute w-screen h-full bg-gray-300 dark:bg-gray-600">
          <client-only>
             <iframe
                width="100%"
                height="100%"
-               frameborder="0"
-               marginheight="0"
-               marginwidth="0"
                title="map"
-               scrolling="no"
                src="https://www.openstreetmap.org/export/embed.html?bbox=27.61370658874512%2C62.88430491020435%2C27.642030715942386%2C62.89283197511212&amp;layer=mapnik&amp;marker=62.888568752496816%2C27.62786865234375"
-               style="filter: grayscale(0.5) contrast(1.2) opacity(0.4)"
+               style="filter: grayscale(0.5) contrast(1.2) opacity(0.4); pointer-events: none"
             ></iframe>
          </client-only>
       </div>
-      <div class="container px-5 py-24 mx-auto flex">
-         <div class="bg-white dark:bg-black rounded-lg p-8 flex flex-col ml-auto mt-10 md:mt-0 relative">
-            <h2 class="text-2xl mb-2 font-medium title-font">Osoite</h2>
-            <div class="leading-relaxed mb-5">
-               <p>Microkatu 1</p>
-               <p>PL 1627</p>
-               <p>70211 Kuopio</p>
+      <div class="w-full content relative py-24">
+         <div class="w-full inline-flex justify-end">
+            <div class="flex flex-col gap-6 py-10 px-16 bg-white dark:bg-black rounded-lg">
+               <p class="font-bold text-2xl">Ota yhteyttä</p>
+               <div>
+                  <p>Microkatu 1</p>
+                  <p>PL 1627</p>
+                  <p>70211 Kuopio</p>
+               </div>
+               <div>
+                  <p>
+                     <span class="pr-2">
+                        <client-only><font-awesome-icon icon="fa fa-phone" /></client-only> </span
+                     >+358 45 1234567
+                  </p>
+               </div>
+               <div>
+                  <p>
+                     <span class="pr-2">
+                        <client-only><font-awesome-icon icon="fa fa-envelope" /></client-only> </span
+                     >pj@serveriry.fi
+                  </p>
+               </div>
             </div>
          </div>
       </div>
@@ -29,8 +41,17 @@
 </template>
 
 <script>
+   import colors from 'tailwindcss/colors';
+   import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
    export default {
       name: 'EmbeddedMap',
+      components: { FontAwesomeIcon },
+      methods: {
+         colors() {
+            return colors;
+         },
+      },
    };
 </script>
 
