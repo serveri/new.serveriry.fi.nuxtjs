1d6dae2b94c8df56e4aef7709d43048c73a5756e
Jeb <jesperkauppinen@gmail.com>
2023-03-12 03:32:26 +0200

    responsive mobile: no text justify

--- app/src/components/langingpage/DescriptionText.vue
+++ app/src/components/langingpage/DescriptionText.vue
@@ -1,7 +1,7 @@
 <template>
    <div class="lg:pt-5 pb-5 max-w-screen-md md: sm:pt-10 pb-10">
       <h1 class="custom-page-title">{{ $t('h1_front-page') }}</h1>
-      <p class="mt-6 text-justify">
+      <p class="mt-6 sm:text-justify">
          Toimimme linkkinä yliopiston ja opiskelijoiden välillä mm. keräämällä ja välittämällä opiskelijapalautetta
          yliopistolle. Tarjoamme opiskelijajäsenillemme ohjausta edunvalvonnallisissa asioissa ja järjestämme opintojen
          oheen vapaa-ajan toimintaa sekä itsenäisesti että yhdessä muiden ainejärjestöjen kanssa. Järjestämme toimintaa
@@ -9,7 +9,7 @@
          tietojenkäsittelytieteen alan yritysten toiminnasta, jakamalla tietoa valmistumisen jälkeisistä
          mahdollisuusista ja helpotamme siten tulevien tietojenkäsittelijöiden erikoistuvaa suuntautumista.
       </p>
-      <p class="mt-6 text-justify">
+      <p class="mt-6 sm:text-justify">
          Serverit ovat legendaarisia hauskanpitäjiä - omien hippaloiden lisäksi valkohaalariset tunnetaankin ahkerina
          kuokkijoina ja keräämme mielellämme kyseenalaista kunniaa opiskelijatapahtumissa ympäri Suomen. Ei siis ihme
          että Serverin tavaramerkkinä pidetään loistavaa yhteishenkeä ja mahtavaa aktiivisuutta, nörtti-imagosta
