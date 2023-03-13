346c3206f7e1d6e4ca7b9ea43518ae2c16dcbf50
Kalle Helvelahti <helvelahti.kalle@gmail.com>
2023-03-13 12:26:01 +0200

    mitä tkt on sivun layouttia tehty

--- app/src/pages/opiskelu/tkt.vue
+++ app/src/pages/opiskelu/tkt.vue
@@ -4,5 +4,228 @@
          <Title>{{ $t('title_what-is-cs') }} - Serveri ry</Title>
       </Head>
       <h1 class="custom-page-title">{{ $t('h1_what-is-cs') }}</h1>
+
+
+   <div class="erkki-video pb-8">
+      <iframe width="1000" height="560" src="https://www.youtube.com/embed/PMIhWO6C_FY" title="YouTube video player" frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+   </div>
+
+   <div>
+      <h4 class="text-lg font-bold">Jos mietit kannattaisiko alaa alkaa opiskelemaan niin olet oikealla sivulla</h4>
+      <p>Tietojenkäsittelytiede on aavistuksen näsäviisaasti ilmaistuna tiedettä tiedon käsittelystä. 
+         Nykyään tietojenkäsittelytiede hyödyntää vahvasti digitaalisia järjestelmiä. 
+         Käytännössä tietojenkäsittelytieteeseen kuuluu siis tiedon keräämiseen, tallentamiseen, välittämiseen, näyttämiseen, muuntamiseen sekä käsittelyyn liittyviä aihepiirejä.
+          Käytännössä parhaan kuvan tietojenkäsittelytieteen tutkinnon rakenteesta Itä-Suomen yliopistossa saat kurssikuvauksista, jotka löydät alta.
+           Kurssit ovat osapuilleen samat Kuopiossa ja Joensuussa, ainut isompi muuttuja on sivuainemahdollisuudet ja se että Serveriläiset opiskelevat Kuopiossa. 
+         (Joensuussa tietojenkäsittelytieteen ainejärjestönä toimii Skripti ry) </p>
+   </div>
+
+
+      <div>
+         <h2 class="mt-8 text-2xl font-extrabold text-center">UEF tietojenkäsittelytieteen opintorakenne</h2>
+      </div>
+
+  <div>
+         <div class="mt-8">
+            <h2 class="text-xl font-extrabold">Yleisopinnot</h2>
+            <p>Yleisopinnot tukevat opiskelua erityisesti ensimmäisenä opiskeluvuotena. </p>
+         </div>
+
+   <table class="mt-4 w-full rounded-sm ">
+      <tr>
+         <th class="text-left p-2"><p class="mt-2 mb-2">Opintojakso</p></th>
+         
+         <th class="text-left p-2"><p>Kurssikuvaus</p></th>
+         
+         <th class="text-left p-2"><p class="break-words">Laajuus (opintopisteet)</p></th>
+      </tr>
+         
+      
+      <tr>
+         <td class="p-2"><p class="break-words">Hops-tietojenkäsittelytiede</p> </td>
+
+         <td class="p-2"><p class="break-words">(Hops = henkilökohtainen opintosuunnitelma)</p> </td>
+         
+         <td class="p-2"><p class="break-words">2</p> </td>
+      </tr>
+      
+      <tr>
+         <td class="p-2"><p class="break-words">Orientaatio yliopisto-opiskeluun</p> </td>
+
+         <td class="p-2"><p class="break-words">Opiskelija tutustuu oppiaine- ja vertaistuutoroinnin, verkkomateriaalin, erilaisten orientaatiotapahtumien sekä 
+            oheismateriaalin avulla yliopistoon opiskeluympäristönä ja oppimisyhteisönä.
+             Erityisesti tutustutaan oman oppiaineen opiskelukäytäntöihin ja opiskelua tukeviin palveluihin.
+             Lisäksi saadaan tietoa akateemisen opiskelun edellyttämistä tiedoista ja taidoista sekä opiskelukyvystä ja sen ylläpitämisestä. </p> </td>
+
+         <td class="p-2"><p class="break-words">1</p> </td>
+      </tr>
+   </table>
+</div>
+
+<div>
+      <div class="mt-8">
+            <h2 class="text-xl font-extrabold">Perusopinnot</h2>
+            <p>Perusopinnot antavat kokonaiskuvan tietojenkäsittelytieteestä ja rakentavat perustan aineopintojen kursseille. </p>
+      </div>
+
+   <table class="mt-4 w-full rounded-sm ">
+      <tr>
+         <th class="text-left p-2"><p class="mt-2 mb-2">Opintojakso</p></th>
+         
+         <th class="text-left p-2"><p>Kurssikuvaus</p></th>
+         
+         <th class="text-left p-2"><p class="break-words">Laajuus (opintopisteet)</p></th>
+      </tr>
+         
+      
+      <tr>
+         <td class="p-2"><p class="break-words">Hops-tietojenkäsittelytiede</p> </td>
+
+         <td class="p-2"><p class="break-words">(Hops = henkilökohtainen opintosuunnitelma)</p> </td>
+         
+         <td class="p-2"><p class="break-words">2</p> </td>
+      </tr>
+      
+      <tr>
+         <td class="p-2"><p class="break-words">Orientaatio yliopisto-opiskeluun</p> </td>
+
+         <td class="p-2"><p class="break-words">Opiskelija tutustuu oppiaine- ja vertaistuutoroinnin, verkkomateriaalin, erilaisten orientaatiotapahtumien sekä 
+            oheismateriaalin avulla yliopistoon opiskeluympäristönä ja oppimisyhteisönä.
+             Erityisesti tutustutaan oman oppiaineen opiskelukäytäntöihin ja opiskelua tukeviin palveluihin.
+             Lisäksi saadaan tietoa akateemisen opiskelun edellyttämistä tiedoista ja taidoista sekä opiskelukyvystä ja sen ylläpitämisestä. </p> </td>
+
+         <td class="p-2"><p class="break-words">1</p> </td>
+      </tr>
+   </table>
+</div>
+
+<div>
+      <div class="mt-8">
+            <h2 class="text-xl font-extrabold">Valinnaiset aineopinnot</h2>
+            <p>Valinnaisia aineopintoja kuuluu tutkintoon vähintään 5 op. Ne sijoittuvat toiseen ja kolmanteen opiskeluvuoteen. 
+               Kurssit käsittelevät tietojenkäsittelyn erilaisia kiinnostavia osa-alueita, jotka tukevat ammatillista osaamista ja 
+               antavat osaltaan pohjaa maisteriopinnoille.  </p>
+      </div>
+
+   <table class="mt-4 w-full rounded-sm ">
+      <tr>
+         <th class="text-left p-2"><p class="mt-2 mb-2">Opintojakso</p></th>
+         
+         <th class="text-left p-2"><p>Kurssikuvaus</p></th>
+         
+         <th class="text-left p-2"><p class="break-words">Laajuus (opintopisteet)</p></th>
+      </tr>
+         
+      
+      <tr>
+         <td class="p-2"><p class="break-words">Hops-tietojenkäsittelytiede</p> </td>
+
+         <td class="p-2"><p class="break-words">(Hops = henkilökohtainen opintosuunnitelma)</p> </td>
+         
+         <td class="p-2"><p class="break-words">2</p> </td>
+      </tr>
+      
+      <tr>
+         <td class="p-2"><p class="break-words">Orientaatio yliopisto-opiskeluun</p> </td>
+
+         <td class="p-2"><p class="break-words">Opiskelija tutustuu oppiaine- ja vertaistuutoroinnin, verkkomateriaalin, erilaisten orientaatiotapahtumien sekä 
+            oheismateriaalin avulla yliopistoon opiskeluympäristönä ja oppimisyhteisönä.
+             Erityisesti tutustutaan oman oppiaineen opiskelukäytäntöihin ja opiskelua tukeviin palveluihin.
+             Lisäksi saadaan tietoa akateemisen opiskelun edellyttämistä tiedoista ja taidoista sekä opiskelukyvystä ja sen ylläpitämisestä. </p> </td>
+
+         <td class="p-2"><p class="break-words">1</p> </td>
+      </tr>
+   </table>
+</div>
+
+<div>
+      <div class="mt-8">
+            <h2 class="text-xl font-extrabold">Kieli- ja viestintäopinnot</h2>
+            <p>Kieli- ja viestintäopinnot tukevat tietojenkäsittelytieteen opintoja ja antavat perustaidot ruotsin ja englannin kielten akateemiselle osaamiselle.</p>
+      </div>
+
+   <table class="mt-4 w-full rounded-sm ">
+      <tr>
+         <th class="text-left p-2"><p class="mt-2 mb-2">Opintojakso</p></th>
+         
+         <th class="text-left p-2"><p>Kurssikuvaus</p></th>
+         
+         <th class="text-left p-2"><p class="break-words">Laajuus (opintopisteet)</p></th>
+      </tr>
+         
+      
+      <tr>
+         <td class="p-2"><p class="break-words">Hops-tietojenkäsittelytiede</p> </td>
+
+         <td class="p-2"><p class="break-words">(Hops = henkilökohtainen opintosuunnitelma)</p> </td>
+         
+         <td class="p-2"><p class="break-words">2</p> </td>
+      </tr>
+      
+      <tr>
+         <td class="p-2"><p class="break-words">Orientaatio yliopisto-opiskeluun</p> </td>
+
+         <td class="p-2"><p class="break-words">Opiskelija tutustuu oppiaine- ja vertaistuutoroinnin, verkkomateriaalin, erilaisten orientaatiotapahtumien sekä 
+            oheismateriaalin avulla yliopistoon opiskeluympäristönä ja oppimisyhteisönä.
+             Erityisesti tutustutaan oman oppiaineen opiskelukäytäntöihin ja opiskelua tukeviin palveluihin.
+             Lisäksi saadaan tietoa akateemisen opiskelun edellyttämistä tiedoista ja taidoista sekä opiskelukyvystä ja sen ylläpitämisestä. </p> </td>
+
+         <td class="p-2"><p class="break-words">1</p> </td>
+      </tr>
+   </table>
+</div>
+
+<div>
+      <div class="mt-8">
+            <h2 class="text-xl font-extrabold">Syventävät opinnot</h2>
+            <p>Tietojenkäsittelytieteen syventäviin opintoihin kuuluu 2 pakollista 6 op kurssia, jotka voi valita tarjolla olevista viidestä kurssista: 
+               Algoritmien suunnittelu ja analysointi, Ohjelmistotuotanto, Tietojenkäsittelytieteen tutkimusmenetelmät, Hahmontunnistus ja Tekoäly. </p>
+      </div>
+
+   <table class="mt-4 w-full rounded-sm ">
+      <tr>
+         <th class="text-left p-2"><p class="mt-2 mb-2">Opintojakso</p></th>
+         
+         <th class="text-left p-2"><p>Kurssikuvaus</p></th>
+         
+         <th class="text-left p-2"><p class="break-words">Laajuus (opintopisteet)</p></th>
+      </tr>
+         
+      
+      <tr>
+         <td class="p-2"><p class="break-words">Hops-tietojenkäsittelytiede</p> </td>
+
+         <td class="p-2"><p class="break-words">(Hops = henkilökohtainen opintosuunnitelma)</p> </td>
+         
+         <td class="p-2"><p class="break-words">2</p> </td>
+      </tr>
+      
+      <tr>
+         <td class="p-2"><p class="break-words">Orientaatio yliopisto-opiskeluun</p> </td>
+
+         <td class="p-2"><p class="break-words">Opiskelija tutustuu oppiaine- ja vertaistuutoroinnin, verkkomateriaalin, erilaisten orientaatiotapahtumien sekä 
+            oheismateriaalin avulla yliopistoon opiskeluympäristönä ja oppimisyhteisönä.
+             Erityisesti tutustutaan oman oppiaineen opiskelukäytäntöihin ja opiskelua tukeviin palveluihin.
+             Lisäksi saadaan tietoa akateemisen opiskelun edellyttämistä tiedoista ja taidoista sekä opiskelukyvystä ja sen ylläpitämisestä. </p> </td>
+
+         <td class="p-2"><p class="break-words">1</p> </td>
+      </tr>
+   </table>
+</div>
+
    </div>
 </template>
+
+<style scoped>
+
+.erkki-video{
+   display: flex;
+   justify-content: center;
+   align-items: center;
+}
+
+th{
+   background-color: rgb(0 119 138 / var(--tw-bg-opacity));
+}
+</style>
