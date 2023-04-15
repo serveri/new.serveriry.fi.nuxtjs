54427b9ec83154ccaa353bf3a6f4c7727b22af7e
Valtteri <valtteri@ikius.com>
2023-04-15 15:56:13 +0300

    navbar functionality

--- app/src/components/navbar/dropdown/DropDownMobile.vue
+++ app/src/components/navbar/dropdown/DropDownMobile.vue
@@ -1,6 +1,6 @@
 <template>
    <!-- Dropdown -->
-   <Menu as="div" class="relative" v-slot="{close}">
+   <Menu as="div" class="relative">
       <MenuButton>
          <!--        :to="localePath(menu.href)"-->
          <nuxt-link
@@ -11,6 +11,7 @@
                'flex items-center justify-start block px-3 py-2 rounded-md text-base font-bold uppercase whitespace-nowrap w-full',
             ]"
             :aria-current="menu.current ? 'page' : undefined"
+            
          >
             {{ $t(menu.name) }}
             <DropDownArrow />
@@ -24,16 +25,17 @@
          leave-from-class="transform opacity-100 scale-100"
          leave-to-class="transform opacity-0 scale-95"
       >
+      <div>
          <MenuItems
             class="absolute right-0 z-10 mt-2 w-full origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none whitespace-nowrap bg-transparent"
-         >
-            <MenuItem v-for="subMenu in menu.subMenu" v-slot="{ active }" :key="subMenu.name">
+          
+            >
+            <MenuItem v-for="subMenu in menu.subMenu" v-slot="{ active, close }" :key="subMenu.name">
                <nuxt-link
-                  @click="close"
                   :to="localePath(subMenu.href)"
                   :class="[
                      active ? 'bg-neutral-100' : '',
-                     'block px-4 py-4 text-lg text-left text-black hover:bg-zinc-600 hover:text-white even:bg-zinc-200 odd:bg-zinc-50 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white dark:even:bg-zinc-800 dark:odd:bg-zinc-700',
+                     'block px-4 py-1 text-lg text-left text-black hover:bg-zinc-600 hover:text-white even:bg-zinc-200 odd:bg-zinc-50 dark:text-gray-200 dark:hover:bg-gray-600 dark:hover:text-white dark:even:bg-zinc-800 dark:odd:bg-zinc-700 w-full',
                   ]"
                   ><span class="nav-dropdown-link" @click="close">
                      {{ $t(subMenu.name) }}
@@ -41,6 +43,7 @@
                >
             </MenuItem>
          </MenuItems>
+      </div>
       </transition>
    </Menu>
 </template>
