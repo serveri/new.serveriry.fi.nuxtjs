c63b113aec80c66db2fc3943386c62b67ab620f9
Jeb <commit@jeb4.dev>
2023-04-29 23:43:14 +0300

    Added form submit logic and submit save to directus

--- app/src/components/partners/ContactFormBusiness.vue
+++ app/src/components/partners/ContactFormBusiness.vue
@@ -5,13 +5,14 @@
          Haluaisitko sinä tai yrityksesi tukea Serveri Ry:n toimintaa ja saada näkyvyyttä opiskelijoiden keskuudessa
          esimerkiksi tapahtumissa ja somessa?
       </p>
-      <form onsubmit="alert('This website is demo')" class="space-y-8">
+      <form class="space-y-8">
          <div>
             <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
                >Yrityksen nimi</label
             >
             <input
                id="subject"
+               v-model="firm_name"
                type="text"
                class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                placeholder="OY yritys AB"
@@ -24,6 +25,7 @@
             >
             <input
                id="email"
+               v-model="firm_contact"
                type="email"
                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                placeholder="name@example.com"
@@ -36,19 +38,50 @@
             >
             <textarea
                id="message"
+               v-model="firm_message"
                rows="6"
                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                placeholder="Millaista yhteistyöta haluaisitte tehdä?"
             ></textarea>
          </div>
-         <button type="submit" class="btn-custom-blue">Lähetä viesti</button>
+         <button type="submit" class="btn-custom-blue" @click="submitForm">Lähetä viesti</button>
       </form>
    </div>
 </template>
 
 <script>
    export default {
-      name: 'ContactFormBusiness',
+      data() {
+         return {
+            firm_name: this.firm_name,
+            firm_contact: this.firm_contact,
+            firm_message: this.firm_message,
+         };
+      },
+
+      async submit() {
+         this.user = await this.submitForm();
+      },
+
+      methods: {
+         async submitForm(e) {
+            e.preventDefault();
+            await fetch('http://10.143.20.7:8081/items/lomake_vastaukset', {
+               headers: {
+                  'Content-Type': 'application/json',
+               },
+               method: 'POST',
+               mode: 'cors',
+               body: JSON.stringify({
+                  firm_message: this.firm_message,
+                  firm_contact: this.firm_contact,
+                  firm_name: this.firm_name,
+               }),
+            });
+            // Redirect to success page
+            this.$router.push('/yrityksille/kiitos');
+         },
+      },
    };
 </script>
 
