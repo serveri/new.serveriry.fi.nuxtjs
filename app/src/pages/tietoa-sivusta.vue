4a8f541c8ae92a3b58929819ddb856b175e857ff
Valtteri <valtteri@ikius.com>
2023-05-03 02:53:25 +0300

    added multi language support

--- app/src/pages/tietoa-sivusta.vue
+++ app/src/pages/tietoa-sivusta.vue
@@ -5,22 +5,22 @@
       </Head>
       <h1 class="custom-page-title">{{ $t('h1_about_site') }}</h1>
       <div>
-         <vue-markdown class="rich-text py-2" :source="about_text" />
+         <vue-markdown class="rich-text py-2" :source="about_text['tietoa_sivustosta_text_' + $i18n.locale]" />
       </div>
    </div>
 </template>
 
 <script setup>
+   import { HandRaisedIcon } from '@heroicons/vue/24/outline';
    import VueMarkdown from 'vue-markdown-render';
    let about_text;
    try {
       let response = await useFetch('https://api.serveri.jeb4.dev/items/tietoa_sivustosta');
-      about_text = response.data.value.data.tietoa_sivustosta_text;
-      console.log('rixhText:', about_text);
+      about_text = response.data.value.data;
    } catch (e) {
       about_text = {
-         fi_text: 'API error',
-         en_text: 'API error',
+         tietoa_sivustosta_text_fi: 'API error',
+         tietoa_sivustosta_text_en: 'API error',
       };
    }
 </script>
