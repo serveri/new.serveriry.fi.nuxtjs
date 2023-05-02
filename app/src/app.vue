ccd7b72cf46477dca651971b650f01e293482e77
Jeb <commit@jeb4.dev>
2023-05-02 17:18:34 +0300

    meta tags

--- app/src/app.vue
+++ app/src/app.vue
@@ -1,4 +1,12 @@
 <template>
+   <Html :lang="$i18n.locale" />
+   <Head>
+      <Meta
+         name="description"
+         content="Itä-Suomen yliopiston Kuopion kampuksen tietojenkäsittelytieteen opiskelijoiden ainejärjestö."
+      />
+      <Title>Serveri ry</Title>
+   </Head>
    <div id="app" class="bg-white dark:bg-zinc-800 min-h-screen overflow-hidden">
       <NavBar />
       <page-content />
