fbf8691c612a1842aab425fc63b3b3713b45ec44
Jeb <commit@jeb4.dev>
2023-05-03 02:58:27 +0300

    removed console log

--- app/src/pages/opiskelu/palaute.vue
+++ app/src/pages/opiskelu/palaute.vue
@@ -6,10 +6,8 @@
       <h1 class="custom-page-title">{{ $t('h1_feedback') }}</h1>
 
       <vue-markdown class="rich-text py-2" :source="text[$i18n.locale + '_text']" />
-
    </div>
 </template>
-
 
 <script setup lang="ts">
    import VueMarkdown from 'vue-markdown-render';
@@ -17,7 +15,6 @@
    try {
       const response = await useFetch('https://api.serveri.jeb4.dev/items/feedback_page');
       text = response.data.value.data;
-      console.log(text);
    } catch (e) {
       text = {
          fi_text: 'API error',
