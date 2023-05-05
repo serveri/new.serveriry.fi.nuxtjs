faec9eb9ba59e1c8a5a43e0de9f86bec5ce03f93
Jeb <commit@jeb4.dev>
2023-05-05 13:54:30 +0300

    merch and history pages

--- app/src/pages/yhdistys/historia.vue
+++ app/src/pages/yhdistys/historia.vue
@@ -5,11 +5,49 @@
       </Head>
       <h1 class="custom-page-title">{{ $t('h1_history') }}</h1>
 
-
-
-
-         <h2 class="pt-9 text-center text-3xl font-extrabold">TULOSSA</h2>
-
-
+      <div v-for="report in reports" :key="report.vuosi">
+         <details class="group py-4">
+            <summary class="flex items-center font-medium cursor-pointer list-none">
+               <span class="transition group-open:rotate-90">
+                  <svg
+                     fill="none"
+                     height="36"
+                     shape-rendering="geometricPrecision"
+                     stroke="currentColor"
+                     stroke-linecap="round"
+                     stroke-linejoin="round"
+                     stroke-width="1.5"
+                     viewBox="0 0 24 24"
+                     width="36"
+                     class="dark:invert -rotate-90"
+                  >
+                     <path d="M6 9l6 6 6-6"></path>
+                  </svg>
+               </span>
+               <h2 class="pl-2 text-2xl">TOIMINTAKERTOMUS {{ report.vuosi }}</h2>
+            </summary>
+            <vue-markdown
+               class="rich-text group-open:animate-fadeIn"
+               :source="report[$i18n.locale + '_toimintakertomus']"
+            />
+         </details>
+      </div>
    </div>
 </template>
+
+<script setup lang="ts">
+   import VueMarkdown from 'vue-markdown-render';
+
+   let reports;
+   try {
+      const response = await useFetch('https://api.serveri.jeb4.dev/items/toimintakertomus');
+      reports = response.data.value.data;
+   } catch (e) {
+      reports = [
+         {
+            fi_toimintakertomus: '# Toimintakertomukset kadoksissa \n try again later or inform sysadmin',
+            en_toimintakertomus: '# Report 2077 \n ### Who lost the history? \n try again later or inform sysadmin',
+         },
+      ];
+   }
+</script>
--- app/src/pages/yhdistys/merch.vue
+++ app/src/pages/yhdistys/merch.vue
@@ -3,6 +3,54 @@
       <Head>
          <Title>{{ $t('title_merch') }} - Serveri ry</Title>
       </Head>
-      <h1 class="custom-page-title">{{ $t('h1_merch') }}</h1>
+      <vue-markdown class="rich-text merch-content" :source="merch[$i18n.locale + '_content']" />
    </div>
 </template>
+
+<script setup lang="ts">
+   import VueMarkdown from 'vue-markdown-render';
+
+   let merch;
+   let updated: Date;
+   try {
+      const response = await useFetch('https://api.serveri.jeb4.dev/items/merch_sivu');
+      merch = response.data.value.data;
+      updated = new Date(merch.date_updated);
+   } catch (e) {
+      merch = {
+         fi_content: '# Mertsi kadoksissa',
+         en_content: '# Merch missing',
+      };
+      updated = new Date();
+   }
+</script>
+
+<style>
+   p {
+      @apply tracking-wide;
+   }
+   .merch-content img {
+      @apply max-w-lg w-full;
+   }
+   .merch-content h2 {
+      @apply font-bold text-3xl mt-4 text-center md:text-left;
+   }
+   .merch-content h3 {
+      @apply text-2xl;
+   }
+   .merch-content h4 {
+      @apply text-xl;
+   }
+
+   .merch-content h6 {
+      @apply text-sm;
+   }
+
+   .merch-content blockquote {
+      @apply text-xl italic font-semibold text-gray-900 dark:text-white;
+   }
+   .merch-content blockquote p::before {
+      content: url('/images/blockquote.svg');
+      padding-right: 1rem;
+   }
+</style>
