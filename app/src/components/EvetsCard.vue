9f553d741ef1814bdf38b02e48b143f5b8610e93
Jeb <commit@jeb4.dev>
2023-05-22 13:07:39 +0300

    moved date options

--- app/src/components/EvetsCard.vue
+++ app/src/components/EvetsCard.vue
@@ -9,7 +9,7 @@
             <h2 class="card-header pt-3 text-2xl font-extrabold">{{ content[$i18n.locale + '_title'] }}</h2>
 
             <p class="news-date font-normal uppercase mt-1 text-xs">
-               {{ content.start_time.toLocaleDateString($i18n.locale, options) }}
+               {{ content.start_time.toLocaleDateString($i18n.locale, { weekday: 'long', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' }) }}
             </p>
 
             <p class="card-content mt-1 mb-3 line-clamp-3">{{ content[$i18n.locale + '_text'] }}</p>
@@ -57,7 +57,6 @@
          required: true,
       },
    });
-   const options = { weekday: 'long', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
 </script>
 
 <style scoped>
