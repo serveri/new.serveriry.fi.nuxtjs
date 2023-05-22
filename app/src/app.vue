de264b917dbe6cbfed95c467a5b625a65b7af806
Jeb <commit@jeb4.dev>
2023-05-22 20:15:15 +0300

    global meta tags

--- app/src/app.vue
+++ app/src/app.vue
@@ -5,6 +5,18 @@
          name="description"
          content="Itä-Suomen yliopiston Kuopion kampuksen tietojenkäsittelytieteen opiskelijoiden ainejärjestö."
       />
+      <Meta>
+         <meta property="og:title" content="Serveri ry" />
+         <meta
+            property="og:description"
+            content="Itä-Suomen yliopiston Kuopion kampuksen tietojenkäsittelytieteen opiskelijoiden ainejärjestö."
+         />
+         <meta
+            property="og:image"
+            content="https://cloud.serveriry.fi/apps/files_sharing/publicpreview/MjiD9378faa3RA6?x=1920&y=577&a=true&file=serverihiiri.png"
+         />
+         <meta property="og:url" content="https://serveriry.fi/" />
+      </Meta>
       <Title>Serveri ry</Title>
    </Head>
    <div id="app" class="bg-white dark:bg-zinc-800 min-h-screen overflow-hidden">
