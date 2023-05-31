fbfcd4b796c360672066f4b1cb7a309fa4dd666d
Jeb <commit@jeb4.dev>
2023-05-31 23:01:16 +0300

    it rekry page

--- app/src/pages/yrityksille/it-rekry.vue
+++ app/src/pages/yrityksille/it-rekry.vue
@@ -3,6 +3,54 @@
       <Head>
          <Title>{{ $t('title_it-rekry') }} - Serveri ry</Title>
       </Head>
-      <h1 class="custom-page-title">{{ $t('h1_it-rekry') }}</h1>
+      <vue-markdown class="rich-text merch-content" :source="merch[$i18n.locale + '_text']" />
    </div>
 </template>
+
+<script setup lang="ts">
+   import VueMarkdown from 'vue-markdown-render';
+
+   let merch;
+   let updated: Date;
+   try {
+      const response = await useFetch('https://api.serveri.jeb4.dev/items/it_rekry');
+      merch = response.data.value.data;
+      updated = new Date(merch.date_updated);
+   } catch (e) {
+      merch = {
+         fi_text: '# IT Rekry',
+         en_text: '# IT Recruitment',
+      };
+      updated = new Date();
+   }
+</script>
+
+<style>
+   p {
+      @apply tracking-wide;
+   }
+   .merch-content img {
+      @apply w-full;
+   }
+   .merch-content h2 {
+      @apply font-bold text-3xl mt-4 text-center md:text-left;
+   }
+   .merch-content h3 {
+      @apply text-2xl;
+   }
+   .merch-content h4 {
+      @apply text-xl;
+   }
+
+   .merch-content h6 {
+      @apply text-sm;
+   }
+
+   .merch-content table {
+      @apply table-auto;
+   }
+
+   th {
+      text-align: left !important;
+   }
+</style>
