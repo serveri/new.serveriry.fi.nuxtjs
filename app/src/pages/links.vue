f5f3644fb705d7e77f8d85280dc6cf47511be1f2
Jeb <commit@jeb4.dev>
2023-06-01 00:21:29 +0300

    useful links from api

--- app/src/pages/links.vue
+++ app/src/pages/links.vue
@@ -3,6 +3,24 @@
       <Head>
          <Title>{{ $t('title_links') }} - Serveri ry</Title>
       </Head>
-      <h1 class="custom-page-title">{{ $t('h1_links') }}</h1>
+      <vue-markdown class="rich-text links-content" :source="links[$i18n.locale + '_content']" />
    </div>
 </template>
+
+<script setup lang="ts">
+   import VueMarkdown from 'vue-markdown-render';
+
+   let links;
+   let updated: Date;
+   try {
+      const response = await useFetch('https://api.serveri.jeb4.dev/items/links');
+      links = response.data.value.data;
+      updated = new Date(links.date_updated);
+   } catch (e) {
+      links = {
+         fi_content: '# Hyödylliset linkit',
+         en_content: '# Useful links',
+      };
+      updated = new Date();
+   }
+</script>
