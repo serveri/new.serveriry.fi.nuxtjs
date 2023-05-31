db6081336923a363d7386a4684be78d5896cc97c
Jeb <commit@jeb4.dev>
2023-06-01 00:28:00 +0300

    fetch data from api

--- app/src/pages/yrityksille/yhteistyo.vue
+++ app/src/pages/yrityksille/yhteistyo.vue
@@ -3,12 +3,33 @@
       <Head>
          <Title>{{ $t('title_collaboration') }} - Serveri ry</Title>
       </Head>
-      <h1 class="custom-page-title">{{ $t('h1_collaboration') }}</h1>
+      <div class="p-4 pb-16 mx-auto max-w-screen-md">
+         <vue-markdown class="rich-text content-style" :source="content[$i18n.locale + '_text']" />
+      </div>
       <section>
          <ContactFormBusiness />
       </section>
    </div>
 </template>
+
 <script setup lang="ts">
    import ContactFormBusiness from '@/components/partners/ContactFormBusiness.vue';
+   import VueMarkdown from 'vue-markdown-render';
+
+   let content;
+   try {
+      const response = await useFetch('https://api.serveri.jeb4.dev/items/yhteistyo_sivu');
+      content = response.data.value.data;
+   } catch (e) {
+      content = {
+         fi_text: '# Yhteistyökumppaniksi?',
+         en_text: '# Collaborate?',
+      };
+   }
 </script>
+
+<style>
+   .content-style p {
+      @apply text-center paragraph;
+   }
+</style>
