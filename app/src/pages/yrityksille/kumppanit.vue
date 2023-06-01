1afd1e16a911fcfc7e210324476cb6e2b816fac7
Jeb <commit@jeb4.dev>
2023-06-01 16:38:42 +0300

    multi language support

--- app/src/pages/yrityksille/kumppanit.vue
+++ app/src/pages/yrityksille/kumppanit.vue
@@ -7,8 +7,11 @@
       <h1 class="custom-page-title">{{ $t('h1_partners') }}</h1>
 
       <p class="mb-8 lg:mb-16 text-center paragraph">
-         Serveri tekee yritysyhteistyötä useiden IT alan yritysten ja muiden opiskelijoille hyödyllisten tahojen kanssa.
-         Tässä on viimeaikaiset tahot jotka ovat olleet esillä jossain tapahtumassamme tai haalareissamme:
+         {{
+            $i18n.locale === 'fi'
+               ? 'Serveri ry tekee yritysyhteistyötä useiden IT alan yritysten ja muiden opiskelijoille hyödyllisten tahojen kanssa. Tässä on viimeaikaiset tahot jotka ovat olleet esillä jossain tapahtumassamme tai haalareissamme:'
+               : 'The Serveri ry works in partnership with a number of IT companies and other organisations that are useful to students. Here are some of the recent ones that have been featured at one of our events or in our overalls:'
+         }}
       </p>
 
       <partner-page-section />
