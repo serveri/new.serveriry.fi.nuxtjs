c6fe5f8eec3b0d50d0084c03aeb08fa35fb61c18
Jeb <commit@jeb4.dev>
2023-06-01 15:23:43 +0300

    get companies from api

--- app/src/pages/yrityksille/yritykset.vue
+++ app/src/pages/yrityksille/yritykset.vue
@@ -5,6 +5,14 @@
       </Head>
 
       <h1 class="custom-page-title">{{ $t('h1_companies') }}</h1>
+      <p class="text-center paragraph pb-6">
+         <!--         if language fin "" else ""-->
+         {{
+            $i18n.locale === 'fi'
+               ? 'Tervetuloa sivustollemme, jossa on listattu kaikki Suomen IT-yritykset. Tämän sivun tarkoituksena on auttaaopiskelijoita löytämään työpaikkoja alan yrityksistä. Selaa listaa ja löydä unelmiesi työpaikka IT-alalta!'
+               : 'Welcome to our website, which lists all Finnish IT companies. The help of this page helps students get jobs from companies in the field. Browse the list for a suitable job and dream position in the IT sector!'
+         }}
+      </p>
       <div class="pb-8">
          <div>
             <h2 class="text-center">Yhteistyökumppanit</h2>
@@ -13,58 +21,14 @@
          <div>
             <div class="py-4">
                <h2 class="text-center">Kuopiossa:</h2>
-               <ul class="flex flex-wrap text-center pt-8">
-                  <li>Ambientia</li>
-                  <li>CGI</li>
-                  <li>Efecte</li>
-                  <li>Etteplan</li>
-                  <li>Innofactor</li>
-                  <li>Loihde</li>
-                  <li>Ponsse</li>
-                  <li>Talent Tree</li>
-                  <li>Tietoevry</li>
-                  <li>Valmet</li>
-                  <li>Huld</li>
-                  <li>Hurja</li>
-                  <!-- Jätä aina 4 jaollinen luku lista elementtejä niin näyttää nätimmältä-->
+               <ul class="grid grid-cols-4 text-center pt-8">
+                  <li v-for="company in kuopio" :key="company">{{ company }}</li>
                </ul>
             </div>
             <div class="py-4">
                <h2 class="text-center">Koko Suomi:</h2>
-               <ul class="flex flex-wrap text-center pt-8">
-                  <li>Alfame</li>
-                  <li>Apotti</li>
-                  <li>F-secure</li>
-                  <li>Fortum</li>
-                  <li>Gofore</li>
-                  <li>Next Games</li>
-                  <li>Nixu</li>
-                  <li>Nokia</li>
-                  <li>Rovio</li>
-                  <li>Sitowise</li>
-                  <li>Supercell</li>
-                  <li>Visma</li>
-                  <li>With Secure</li>
-                  <li>Yle</li>
-                  <li>Siili solutions</li>
-                  <li>Nordea</li>
-                  <li>Solita</li>
-                  <li>Loimu</li>
-                  <li>Abloy</li>
-                  <li>Blancco</li>
-                  <li>Digia</li>
-                  <li>Futurice</li>
-                  <li>Reaktor</li>
-                  <li>accenture</li>
-                  <li>Nitor</li>
-                  <li>boogie</li>
-                  <li>Vaimo</li>
-                  <li>Qt</li>
-                  <li>netum</li>
-                  <li>Vincit</li>
-                  <li>Pinja</li>
-                  <li></li>
-                  <!-- Jätä aina 4 jaollinen luku lista elementtejä niin näyttää nätimmältä-->
+               <ul class="grid grid-cols-4 text-center pt-8">
+                  <li v-for="company in finland" :key="company">{{ company }}</li>
                </ul>
             </div>
          </div>
@@ -72,13 +36,24 @@
    </div>
 </template>
 
-<script>
+<script setup lang="ts">
    import PartnerPageSection from '@/components/partners/PartnerPageSection.vue';
 
-   export default {
-      name: 'Yritykset',
-      components: { PartnerPageSection },
-   };
+   let data;
+   let kuopio: string[];
+   let finland: string[];
+   try {
+      const response = await useFetch('https://api.serveri.jeb4.dev/items/it_comppanies');
+      data = response.data.value.data;
+      // format data to array and remove dashes
+      kuopio = data.kuopio.split('\n').map((item: string) => item.replace('- ', ''));
+      finland = data.finland.split('\n').map((item: string) => item.replace('- ', ''));
+   } catch (e) {
+      data = {
+         kuopio: 'loading companies failed',
+         finland: 'loading companies failed',
+      };
+   }
 </script>
 
 <style scoped>
@@ -90,5 +65,6 @@
    li {
       flex: 1 0 20%;
       margin: 5px 30px;
+      @apply dark:text-white;
    }
 </style>
