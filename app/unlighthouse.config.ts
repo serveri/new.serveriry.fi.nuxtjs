00ecb6621dafcc0eb74fa8c4de4eafe8f129b3fa
Jeb <commit@jeb4.dev>
2024-01-09 10:32:29 +0200

    unlighthouse test config

---
+++ app/unlighthouse.config.ts
@@ -0,0 +1,11 @@
+// unlighthouse.config.ts
+export default {
+   // show the browser window
+   puppeteerOptions: {
+      headless: true,
+   },
+   // limit the number of concurrent tabs
+   puppeteerClusterOptions: {
+      maxConcurrency: 8,
+   },
+};
